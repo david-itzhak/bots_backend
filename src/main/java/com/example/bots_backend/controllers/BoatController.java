63c30b0d95c5b7143a552340b08d4b3178f68c04
David Itzhak <daviditzhak7@gmail.com>
2021-07-27 20:45:54 +0300

    Fix bug with endpoint.

--- src/main/java/com/example/bots_backend/controllers/BoatController.java
+++ src/main/java/com/example/bots_backend/controllers/BoatController.java
@@ -13,24 +13,21 @@ import java.util.List;
 /**
  * @author Dmitry Itskov
  */
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequiredArgsConstructor
 @Log4j2
 public class BoatController {
 
-    private static final String NEW_BOAT_URL = "/newBoat";
     public static final String BOATS_URL = "/boats";
 
     private final BoatService boatService;
 
-    @CrossOrigin
-    @PostMapping(value = NEW_BOAT_URL, produces = "application/json")
+    @PostMapping(value = BOATS_URL, produces = "application/json")
     public BoatResult createBot(@Valid @RequestBody BoatDto boatDto) {
         return boatService.createBoat(boatDto);
     }
 
-    @CrossOrigin
     @GetMapping(value = BOATS_URL, produces = "application/json")
     public List<BoatResult> getAllBot() {
         return boatService.getAllBoat();
